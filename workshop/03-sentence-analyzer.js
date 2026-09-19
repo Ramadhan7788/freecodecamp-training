d3ff2d8c88a6859aa17006fef729d6c34bd200d4
Muhammad Ramadhan <muhramadhan7788@gmail.com>
2026-09-19 21:41:30 +0800

    feat(workshop 03-sentence-analyzer): add getPunctuationCount function

--- workshop/03-sentence-analyzer.js
+++ workshop/03-sentence-analyzer.js
@@ -1,5 +1,6 @@
 const vowels = "aeiou";
 const consonant = 'bcdfghjklmnpqrstvwxyz';
+const punctuation = ".,!?;:-()[]{}\"'–";
 
 const getVowelCount = function (sentence) {
     let count = 0;
@@ -16,12 +17,32 @@ const getConsonantCount = function (sentence) {
     let count = 0;
 
     for (const char of sentence.toLowerCase()) {
-        if (!vowels.includes(char) && character.includes(char)) {
+        if (consonant.includes(char)) {
             count++;
         }
     }
     return count;
 }
 
-console.log(getVowelCount("Apples are tasty fruits"));
-console.log(getConsonantCount("Apples are tasty fruits"));
+const getPunctuationCount = function (sentence) {
+    let count = 0;
+
+    for (const char of sentence.toLowerCase()) {
+        if (punctuation.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+const consonantCount = getConsonantCount("Coding is fun");
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Vowel Count: ${vowelCount}`);
+console.log(`Consonant Count: ${consonantCount}`);
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+
+const kalimat = 'ada apa dengan dunia'
+
+console.log(kalimat.split(' '))
