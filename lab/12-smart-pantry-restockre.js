da3bf5c08dd614908b9018f0d1e5bc26575b8d41
Muhammad Ramadhan <muhramadhan7788@gmail.com>
2026-09-24 18:55:24 +0800

    chore(lab 12-smart-pantry-restocker) add parseShipment function

--- lab/12-smart-pantry-restockre.js
+++ lab/12-smart-pantry-restockre.js
@@ -11,3 +11,28 @@ const rawData = [
 	"D43|Pineapples|0|2027-01-01",
 	"E54|Peppers|-1|2027-01-01|fridge"
 ];
+
+const parseShipment = function (arr) {
+	const newData = [];
+	const seenSkus = new Set();
+
+	for (let i = 0; i < arr.length; i++) {
+		const product = arr[i].split('|');
+		const sku = product[0];
+		const name = product[1];
+		const qty = +product[2];
+		const expires = product[3];
+		const zone = product[4] ?? 'general';
+
+		const productInfo = {sku, name, qty, expires, zone};
+
+		if (!seenSkus.has(sku)) {
+			seenSkus.add(sku);
+			newData.push(productInfo);
+		}
+
+	}
+	return newData;
+};
+
+parseShipment(rawData);
