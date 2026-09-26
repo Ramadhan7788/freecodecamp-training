26a1bf08ca2ca04772bb52cfb0b911812dcd69a2
Muhammad Ramadhan <muhramadhan7788@gmail.com>
2026-09-26 17:43:20 +0800

    chore(fundamentals-rev/lab 01-reverse-a-string) finalize project

---
+++ fundamentals-rev/lab/01-reverse-a-string.js
@@ -0,0 +1,6 @@
+const reverseString = function (word) {
+	return word.split('').reverse().join('');
+}
+
+const coba = reverseString('makan');
+console.log(coba);
---
+++ variables-strings/lab/01-javascript-trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm yor coding fun fact guide!");
+
+const botName = 'Lixia';
+const botLocation = 'Your pc';
+const favoriteLanguage = 'JavaScript';
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programing language is ${favoriteLanguage}`);
+
+let codingFact = `${favoriteLanguage} is the most popular programming language for frontend development.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} was originally created in just 10 days in May 1995 by Brendan Eich while he was working at Netscape.`
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} was first named Mocha, then changed to LiveScript, before finally being officially named ${favoriteLanguage} to boost its popularity (capitalizing on the fame of the Java language at the time).`
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
